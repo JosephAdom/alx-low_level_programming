13bd0b439e8a005f5e30479244ba68df664c2aa1
JosephAdom <adomjoseph123@gmail.com>
2022-12-13 16:45:37 +0000

    Printing sum of integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @a: integer 1
+ * @b: integer 2
+ * Return: Always 0 (success)
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
