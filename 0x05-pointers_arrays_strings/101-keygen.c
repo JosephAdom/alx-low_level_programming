0b69898c4359ffc1fa1d8d7eb621591601027abd
JosephAdom <adomjoseph123@gmail.com>
2022-12-19 17:53:15 +0000

    Task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int pass[100];
+	int i, num, n;
+
+	num = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		num += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 num) - '0' < 78)
+		{
+			n = 2772 - num - '0';
+			num += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
