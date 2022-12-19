0f37e84a6d2d9d772b1ee6018317b2abd1cda5e1
JosephAdom <adomjoseph123@gmail.com>
2022-12-19 16:46:34 +0000

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * put_half - prints half of a string
+ * followed by a new line
+ * @str: string to be printed
+ */
+
+void puts_half(char *str)
+{
+	int num, n, i;
+
+	num = 0;
+
+	while (str[num] != '\0')
+	{
+		num++;
+	}
+
+	if (num % 2 == 0)
+	{
+		for (i = num / 2; str[1] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	} else if (num % 2)
+	{
+		for (n = (num - 1) / 2; n < num - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
+	_putchar('\n');
+}
