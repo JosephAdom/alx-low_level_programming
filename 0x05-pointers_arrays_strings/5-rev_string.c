61a73139c1b026ec686cdf079a686af73ac14f8b
JosephAdom <adomjoseph123@gmail.com>
2022-12-19 16:23:21 +0000

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,21 +8,22 @@
 void rev_string(char *s)
 {
 	char tmp;
-	int i, num1, num2;
+	int i, len, len1;
 
-	num1 = 0;
-	num2 = 0;
+	len = 0;
+	len1 = 0;
 
-	while (s[num1] != '\0')
+	while (s[len] != '\0')
 	{
-		num1++;
+		len++;
 	}
-	num2 = num1 - 1;
 
-	for (i = 0; i < num1 / 2; i++)
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
 	{
 		tmp = s[i];
-		s[i] s[num2];
-		s[num2--] = tmp;
+		s[i] s[len1];
+		s[len1--] = tmp;
 	}
 }
