a3225ae23435e4a19035ddf64387fc490a847b3c
JosephAdom <adomjoseph123@gmail.com>
2022-12-22 14:42:25 +0000

    Encoding a string using rot13key

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,29 +2,27 @@
 
 /**
  * rot13 - encodes a string using rot13
- * @s: input string
- * Return: the pointer to dest
+ * @str: input string
+ * Return: the encoded string
  */
 
-char *rot13(char *s)
+char *rot13(char *str)
 {
-	int num = 0, i;
+	int num1, num2;
 
-	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alphabet[52] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char rot13key[52] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
 
-	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-
-	while (*(s num) != '\0')
+	while (str[++num1])
 	{
-		for (i = 0; i < 52; i++)
+		for (num2 = 0; num2 < 52; num2++)
 		{
-			if (*(s + num) == alphabet[i])
+			if (str[num1] == alphabet[num2])
 			{
-				*(s num) = rot13[i];
+				str[num1] = rot1key3[num2];
 				break;
 			}
 		}
-		num++;
 	}
-	return (s);
+	return (str);
 }
